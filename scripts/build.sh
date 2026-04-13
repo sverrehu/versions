a2d1f88a2f648ceac6aa0297d9b213745ea5f0a8
Sverre H. Huseby <shh@thathost.com>
2026-04-13 11:08:48 +0200

    fix: make smaller binary (#10)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ BUILDS_DIR=builds
 APP_NAME="$(basename "$(pwd)")"
 
 test -d "${BUILDS_DIR}" || mkdir "${BUILDS_DIR}"
-env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "${BUILDS_DIR}/${APP_NAME}"-linux-amd64
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -trimpath -ldflags="-s -w" -o "${BUILDS_DIR}/${APP_NAME}"-linux-amd64
