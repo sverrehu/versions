5ee1f6c0adc3b9fcb7b6e3ccbc40abd3e257afa6
Sverre H. Huseby <shh@thathost.com>
2026-04-13 09:19:02 +0200

    fix: set CGO_ENABLED=0

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ BUILDS_DIR=builds
 APP_NAME="$(basename "$(pwd)")"
 
 test -d "${BUILDS_DIR}" || mkdir "${BUILDS_DIR}"
-env GOOS=linux GOARCH=amd64 go build -o "${BUILDS_DIR}/${APP_NAME}"-linux-amd64
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "${BUILDS_DIR}/${APP_NAME}"-linux-amd64
