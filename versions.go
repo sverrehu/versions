693b301d616978b782e5fd93f532b853fd2b575a
Sverre H. Huseby <shh@thathost.com>
2026-04-14 09:41:31 +0200

    fix: enable port override from command line

--- versions.go
+++ versions.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/sverrehu/gotest/versions/internal/webserver"
 )
 
 func main() {
-	err := webserver.Run()
+	port := flag.Int("port", 8086, "web server listening port for HTTP")
+	flag.Parse()
+	err := webserver.Run(*port)
 	if err != nil {
 		panic(err)
 	}
