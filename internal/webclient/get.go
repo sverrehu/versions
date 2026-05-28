dfb8712c55043d3558bc0ee8f471e06154df1eb6
Sverre H. Huseby <shh@thathost.com>
2026-05-28 12:24:09 +0200

    fix: robustify webclient.Get (#100)

--- internal/webclient/get.go
+++ internal/webclient/get.go
@@ -1,7 +1,6 @@
 package webclient
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -15,24 +14,44 @@ var client = &http.Client{
 	Timeout: 60 * time.Second,
 }
 
+type HTTPError struct {
+	StatusCode int
+	Status     string
+	Body       string
+}
+
+func (e *HTTPError) Error() string {
+	if e.Body == "" {
+		return e.Status
+	}
+	return fmt.Sprintf("%s: %s", e.Status, e.Body)
+}
+
 func Get(url string, credentials *config.Credentials) (string, error) {
 	log.Printf("fetching %s", url)
-	req, _ := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
 	addCredentials(req, credentials)
 	resp, err := client.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
 	if resp.StatusCode == http.StatusNotFound {
 		return "", nil
 	}
 	if resp.StatusCode != http.StatusOK {
-		return "", errors.New(resp.Status)
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
+		return "", &HTTPError{
+			StatusCode: resp.StatusCode,
+			Status:     resp.Status,
+			Body:       string(body),
+		}
 	}
 	return string(body), nil
 }
